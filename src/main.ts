4f029518b53351acee6ac1990a681193036aac74
Yuichi Tanaka <yuichielectric@github.com>
2020-11-28 10:38:32 +0900

    Compress

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,26 @@
 import path = require('path');
 import chokidar = require('chokidar');
+import child_process = require('child_process');
+import util = require('util');
+
+const exec = util.promisify(child_process.exec);
 const watcher = chokidar.watch('/Users/yuichielectric/Downloads', {
     persistent: true,
     ignoreInitial: true,
     depth: 0
 });
+
+async function compress(filepath: string) {
+    try {
+        await exec(`/usr/local/bin/gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile="${filepath}".optimized "${filepath}"`);
+        await exec(`mv ${filepath}.optimized ${filepath}`);
+    } catch(error) {
+        console.error(error);
+    }
+}
+
 watcher.on('add', filepath => {
     if (path.extname(filepath).toLowerCase() === '.pdf') {
-        console.log(`File ${filepath} has been added`);
-    }
-});
-
-watcher.on('change', filepath => {
-    if (path.extname(filepath).toLowerCase() === '.pdf') {
-        console.log(`File ${filepath} has been changed`);
+        compress(filepath);
     }
 });
